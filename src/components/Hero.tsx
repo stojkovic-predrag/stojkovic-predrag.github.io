23269b6217506e6f79898c9c001f558882751c49
Predrag Stojkovic <predragstojkovic.jagodina@gmail.com>
2023-09-18 20:46:26 +0200

    Added smaller profile image on small screens

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,12 +3,21 @@ import { MdExpandMore } from 'react-icons/md';
 import Image from 'next/image';
 
 import profileImage from '../../public/pedja_resized.png';
+import profileSmall from '../../public/profileSmall.png';
 import Link from 'next/link';
 
 const Hero = () => {
   return (
     <div className='container sm:px-0 px-2 mx-auto h-[80vh] flex flex-col mb-16 mt-10'>
       <div className='max-w-6xl w-full h-full mx-auto flex sm:flex-row flex-col sm:justify-between justify-center items-center'>
+        <div className='mx-auto rounded-full h-24 w-24 overflow-hidden mb-16 border-gray-200 border-2 sm:hidden block mt-10'>
+          <Image
+            src={profileSmall}
+            objectFit='cover'
+            className='h-full w-full'
+            alt='Predrag Stojkovic'
+          />
+        </div>
         <div className='w-7/12 sm:mb-0 mb-8 sm:text-left text-center'>
           <h2 className='lg:text-6xl font-extrabold mb-4 sm:text-4xl text-3xl'>
             You are in need of skilled{' '}
